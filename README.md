3cc150519ca1a4ceb8f184d666e639a30d289fdb
Mihai Tincu <mihai7tincu@gmail.com>
2024-05-09 22:14:41 +0300

    footer

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+cd D:\github\webstore
+VS: Angular Language Service
+node -v
+npm install -g @angular/cli
+ng generate component home
+ng serve
+http://localhost:4200
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent, RouterOutlet],
+  imports: [
+    FooterComponent,
+    NavBarComponent,
+    RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
